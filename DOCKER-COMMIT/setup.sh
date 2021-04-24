1206c1c58a79313ac6f498a98b3fe568adc6884b
kausgang <write.to.kaustav@gmail.com>
2021-04-24 19:25:53 -0400

    docker commit

---
+++ DOCKER-COMMIT/setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# RUN THIS IF YOU HAVE TO ADD SUDO BEFORE DOCKER COMMANDS
+# https://docs.docker.com/engine/install/linux-postinstall/
+
+# sudo groupadd docker
+# sudo usermod -aG docker ${USER}
+# su -s ${USER}
+
+docker stop worldEvents-container
+docker rm worldEvents-container
+
+docker run -d --name worldEvents-container mongo
+
+cd ../DATABASE
+for f in *.json; do
+
+    # https://www.cyberciti.biz/faq/linuxunix-move-file-starting-with-a-dash/
+    docker cp -- $f worldEvents-container:/usr/src/_$f 
+    docker exec worldEvents-container mongoimport -d worldEvents -c collection_world_events --file /usr/src/_$f
+  
+done
+
+
+# create new image based on the container-with-data
+# docker commit worldEvents-container iem22/new_worldEvents-container:v1
